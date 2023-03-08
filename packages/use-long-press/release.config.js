43b8fde88ccd76c37804e2dde84dedc08b9bd98b
minwork <20630104+minwork@users.noreply.github.com>
2023-03-08 14:21:19 +0100

    build(use-long-press): Setup semantic release automation
    - Update missing entries in use-long-press package.json

---
+++ packages/use-long-press/release.config.js
@@ -0,0 +1,62 @@
+// npx semantic-release --no-ci --branch=mono-repo-scripts --extends=./apps/${appName}/release.config.js --debug
+const folder = 'packages/';
+const appName = 'use-long-press';
+
+const commitAnalyzerOptions = {
+  preset: 'conventionalcommits',
+  parserOpts: {
+    mergePattern: /^Merged in (.*) \(pull request #(\d+)\)$/,
+    mergeCorrespondence: ['branch', 'id'],
+  },
+  releaseRules: [
+    { type: 'docs', scope: 'README', release: 'patch' },
+    { type: 'refactor', release: 'patch' },
+    { type: 'style', release: 'patch' },
+    { type: 'perf', release: 'patch' },
+    { type: 'build', release: 'patch' },
+  ],
+};
+
+module.exports = {
+  name: `${appName}`,
+  pkgRoot: `dist/${folder}${appName}`,
+  tagFormat: `${appName}-v$\{version}`,
+  commitPaths: [`${folder}${appName}/*`, 'package.json', 'nx.json'],
+  plugins: [
+    ['@semantic-release/commit-analyzer', commitAnalyzerOptions],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        ...commitAnalyzerOptions,
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'chore', section: 'Chores' },
+            { type: 'docs', hidden: true },
+            { type: 'style', hidden: true },
+            { type: 'refactor', section: 'Refactoring' },
+            { type: 'build', section: 'Build config' },
+            { type: 'perf', hidden: true },
+            { type: 'test', hidden: true },
+          ],
+        },
+      },
+    ],
+    ['@semantic-release/changelog', { changelogFile: `./${folder}${appName}/CHANGELOG.md` }],
+    [
+      '@semantic-release/exec',
+      {
+        // prepareCmd: `yarn test:${appName} && yarn build:${appName}`,
+        //publishCmd: `./docker/build.sh ${appName}`,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [`./${folder}${appName}/CHANGELOG.md`],
+        message: `chore(release): Release ${appName} v$\{nextRelease.version} [skip ci]`,
+      },
+    ],
+  ],
+};
