2aee4a84c27cd93f9324816c7eeac87b072a53cc
minwork <20630104+minwork@users.noreply.github.com>
2025-03-16 18:05:39 +0100

    ci(pr): Rewrite PR verify pipeline

--- packages/react-interval-hook/vite.config.ts
+++ packages/react-interval-hook/vite.config.ts
@@ -68,6 +68,10 @@ export default defineConfig({
       reportsDirectory: '../../coverage/packages/react-interval-hook',
       // Cover only lib files
       include: ['src/lib/**/*'],
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
     },
   },
 });
--- packages/use-long-press/vite.config.ts
+++ packages/use-long-press/vite.config.ts
@@ -69,6 +69,10 @@ export default defineConfig({
       reportsDirectory: '../../coverage/packages/use-long-press',
       // Cover only lib files
       include: ['src/lib/**/*'],
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
     },
   },
 });
